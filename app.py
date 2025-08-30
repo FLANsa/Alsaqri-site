07eedb5bb91a430c33779e2e02773096be44728e
manaf <sgmalajl@liverpool.ac.uk>
2025-08-30 22:28:28 +0300

    Change barcode layout to vertical design: company name at top, barcode in middle, details at bottom

--- app.py
+++ app.py
@@ -523,11 +523,11 @@ def generate_barcode(phone_number, battery_age=None):
     filename = f"static/barcodes/{phone_number}"
     barcode_path = barcode_instance.save(filename, options)
     
-    # Convert the saved image to sticker size (7cm x 3.5cm)
+    # Convert the saved image to sticker size (5cm x 7cm - vertical layout)
     img = Image.open(barcode_path)
     # Convert cm to pixels (1cm = 37.795276 pixels at 96 DPI)
-    width_px = int(7.0 * 37.795276)   # 7cm width
-    height_px = int(3.5 * 37.795276)  # 3.5cm height
+    width_px = int(5.0 * 37.795276)   # 5cm width
+    height_px = int(7.0 * 37.795276)  # 7cm height
     img = img.resize((width_px, height_px), Image.Resampling.LANCZOS)
     img.save(barcode_path)
     
